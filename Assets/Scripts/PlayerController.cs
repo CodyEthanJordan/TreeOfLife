b3b2c92e7f15f0e4aa258b86fedff0bdad67217d
CodyEthanJordan <codyethanjordan@users.noreply.github.com>
2020-04-19 05:37:51 -0400

    Reorganizing

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,109 +6,90 @@ namespace Assets.Scripts
 {
     public class PlayerController : MonoBehaviour
     {
-        public float Speed;
-        public float JumpVelocity;
-        public float LungeVelocity;
-        public float FallMultiplier;
-        public float LowGravFactor;
-        public float DashSpeed;
-        public float DashTime;
-
-
         [SerializeField]
-        private Transform groundCheck;
-
-        private bool canJump;
-        private int dashDirection = 0;
-        private float dashTimer = 10000;
-
-        private Rigidbody2D rb;
-        private SpriteRenderer sr;
-        private Animator anim;
-
+        private SwordsmanController sc;
+       
         void Awake()
         {
-            rb = GetComponent<Rigidbody2D>();
-            sr = GetComponent<SpriteRenderer>();
-            anim = GetComponent<Animator>();
-        }
 
-        void Start()
-        {
-
-        }
-
-        public void Die()
-        {
-            anim.SetTrigger("Die");
         }
 
         void Update()
         {
-            //check for jump
-            var collider = Physics2D.OverlapCircle(groundCheck.position, 0.12f);
-            if (collider is null)
+            if (Input.GetButtonDown("Jump"))
             {
-                canJump = false;
-            }
-            else
-            {
-                canJump = true;
+                Debug.Log("Jump");
+                sc.Jump();
             }
 
-            if (Input.GetButtonDown("Jump") && canJump)
-            {
-                rb.velocity = Vector2.up * JumpVelocity;
-                canJump = false;
-            }
+            //if (rb.velocity.y < 0)
+            //{
+            //    rb.velocity += Vector2.up * Physics2D.gravity.y * (FallMultiplier - 1) * Time.deltaTime;
+            //}
+            //else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+            //{
+            //    rb.velocity += Vector2.up * Physics2D.gravity.y * (LowGravFactor - 1) * Time.deltaTime;
+            //}
 
-            if (rb.velocity.y < 0)
-            {
-                rb.velocity += Vector2.up * Physics2D.gravity.y * (FallMultiplier - 1) * Time.deltaTime;
-            }
-            else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
-            {
-                rb.velocity += Vector2.up * Physics2D.gravity.y * (LowGravFactor - 1) * Time.deltaTime;
-            }
+            //float horizontalVelocity = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
+            //anim.SetFloat("Speed", Mathf.Abs(horizontalVelocity));
 
-            float horizontalVelocity = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
-            anim.SetFloat("Speed", Mathf.Abs(horizontalVelocity));
+            //if (horizontalVelocity > 0)
+            //{
+            //    sr.flipX = true;
+            //}
+            //else if (horizontalVelocity < 0)
+            //{
+            //    sr.flipX = false;
+            //}
 
-            if (horizontalVelocity > 0)
-            {
-                sr.flipX = true;
-            }
-            else if (horizontalVelocity < 0)
-            {
-                sr.flipX = false;
-            }
+            //rb.velocity = new Vector2(horizontalVelocity, rb.velocity.y);
 
-            rb.velocity = new Vector2(horizontalVelocity, rb.velocity.y);
+            //lungeTimer += Time.deltaTime;
+            //if (lungeTimer >= LungeCooldown)
+            //{
 
-            if (Input.GetButtonDown("Lunge"))
-            {
-                anim.SetTrigger("Lunge");
-                dashDirection = sr.flipX ? 1 : -1;
-                dashTimer = 0;
-            }
+            //    if (Input.GetButtonDown("Lunge"))
+            //    {
+            //        anim.SetTrigger("Lunge");
+            //        dashDirection = sr.flipX ? 1 : -1;
+            //        dashTimer = 0;
+            //        lungeTimer = 0;
+            //    }
+            //    if (Input.GetButtonDown("LungeRight"))
+            //    {
+            //        anim.SetTrigger("Lunge");
+            //        dashDirection = 1;
+            //        dashTimer = 0;
+            //        lungeTimer = 0;
+            //    }
+            //    if (Input.GetButtonDown("LungeLeft"))
+            //    {
+            //        anim.SetTrigger("Lunge");
+            //        dashDirection = -1;
+            //        dashTimer = 0;
+            //        lungeTimer = 0;
+            //    }
+            //}
 
-            if(Input.GetKeyDown(KeyCode.K))
-            {
-                Die();
-            }
 
-            dashTimer += Time.deltaTime;
-            if (dashDirection != 0)
-            {
-                if (dashTimer >= DashTime)
-                {
-                    dashDirection = 0;
-                }
-                else
-                {
-                    rb.velocity += new Vector2(DashSpeed * dashDirection, 0);
-                }
-            }
+            //if(Input.GetKeyDown(KeyCode.K))
+            //{
+            //    Die();
+            //}
+
+            //dashTimer += Time.deltaTime;
+            //if (dashDirection != 0)
+            //{
+            //    if (dashTimer >= DashTime)
+            //    {
+            //        dashDirection = 0;
+            //    }
+            //    else
+            //    {
+            //        rb.velocity += new Vector2(DashSpeed * dashDirection, 0);
+            //    }
+            //}
 
 
         }
---
+++ Assets/Scripts/SwordsmanController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class SwordsmanController : MonoBehaviour
+    {
+        public float Speed;
+        public float JumpVelocity;
+        public float LungeCooldown;
+        public float FallMultiplier;
+        public float LowGravFactor;
+        public float DashSpeed;
+        public float DashTime;
+        public bool Dead;
+
+        [SerializeField]
+        private Transform groundCheck;
+
+        protected bool canJump;
+        protected int dashDirection = 0;
+        protected float dashTimer = 10000;
+        protected float lungeTimer = 1000;
+
+        protected Rigidbody2D rb;
+        protected SpriteRenderer sr;
+        protected Animator anim;
+
+        void Awake()
+        {
+            rb = GetComponent<Rigidbody2D>();
+            sr = GetComponent<SpriteRenderer>();
+            anim = GetComponent<Animator>();
+        }
+
+        public void Die()
+        {
+            anim.SetTrigger("Die");
+            Dead = true;
+        }
+
+        public void Jump()
+        {
+            if(!canJump)
+            {
+                return;
+            }
+
+            rb.velocity = Vector2.up * JumpVelocity;
+            canJump = false;
+        }
+
+        private void Update()
+        {
+            Debug.Log("yo");
+            //check for jump
+            var collider = Physics2D.OverlapCircle(groundCheck.position, 0.12f);
+            if (collider is null)
+            {
+                canJump = false;
+            }
+            else
+            {
+                canJump = true;
+            }
+        }
+    }
+}
