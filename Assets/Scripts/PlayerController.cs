09698bd6a85f1af3510b9da3c402c02debb146ba
CodyEthanJordan <codyethanjordan@users.noreply.github.com>
2020-04-18 20:46:43 -0400

    Getting in the zone

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,15 +5,53 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float Speed;
-    // Start is called before the first frame update
+    public float JumpVelocity;
+    public float FallMultiplier;
+    public float LowGravFactor;
+
+    [SerializeField]
+    private Transform groundCheck;
+
+    private bool canJump;
+
+    private Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
+        //check for jump
+        var collider = Physics2D.OverlapCircle(groundCheck.position, 0.12f);
+        if(collider is null)
+        {
+            canJump = false;
+        }
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = Vector2.up * JumpVelocity;
+            canJump = false;
+        }
+
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (FallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (LowGravFactor - 1) * Time.deltaTime;
+        }
+
+        rb.velocity = new Vector2(0, 0);
+
         
     }
 }
